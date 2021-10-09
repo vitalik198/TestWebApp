bd67ed9ecefe7b1d37ae26d85d1f2d2e884148a4
Чабан Виталий Андреевич <vitalik271198@gmail.com>
2021-10-09 16:08:52 +0300

    Revert "Revert "fixes""
    This reverts commit 51e1cdcb383b0c931379a223e9236f39753dce3d.

--- MyTestWebApp/Migrations/20211003121328_useridtoname.Designer.cs
+++ MyTestWebApp/Migrations/20211003121328_useridtoname.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyTestWebApp.Context;
 
 namespace MyTestWebApp.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20211003121328_useridtoname")]
+    partial class useridtoname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
